be1e8ef493022dcae99fc867d313e65b21544239
YounSangMu <37579735+YounSangMu@users.noreply.github.com>
2025-01-15 00:50:08 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -437,7 +437,6 @@ if st.button('제출', type='primary') :
     df_scores = df_scores.sort_values(by=["총점", "성향점수", "연애점수"], ascending=False)
 
     real_df_total = df_total
-    st.write(basic_check)
 
     if basic_check[0] == "500" : 
         real_df_total = real_df_total[real_df_total["page"] <= 500]
@@ -455,6 +454,7 @@ if st.button('제출', type='primary') :
         real_df_total = real_df_total[real_df_total["author_cd"] != 'KSY']
 
     real_df_total = real_df_total.sort_values(by=["총점", "성향점수", "연애점수"], ascending=False)
+    st.dataframe(real_df_total)
     target_author = real_df_total.head(1)
 
     st.header(f"{target_author['author_nm'].iloc[0]} 🎉")
