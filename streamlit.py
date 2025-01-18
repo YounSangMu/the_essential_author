1d8c82b0a7f99cd57384637aa9ceea103531238d
YounSangMu <37579735+YounSangMu@users.noreply.github.com>
2025-01-18 16:51:09 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -30,19 +30,19 @@ import shutil
 #     </script>
 #     """
 
-    # Insert the script in the head tag of the static template inside your virtual
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    logging.info(f'editing {index_path}')
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID): 
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  
-        else:
-            shutil.copy(index_path, bck_index)  
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_JS)
-        index_path.write_text(new_html)
+    # # Insert the script in the head tag of the static template inside your virtual
+    # index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    # logging.info(f'editing {index_path}')
+    # soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    # if not soup.find(id=GA_ID): 
+    #     bck_index = index_path.with_suffix('.bck')
+    #     if bck_index.exists():
+    #         shutil.copy(bck_index, index_path)  
+    #     else:
+    #         shutil.copy(index_path, bck_index)  
+    #     html = str(soup)
+    #     new_html = html.replace('<head>', '<head>\n' + GA_JS)
+    #     index_path.write_text(new_html)
 
 
 # inject_ga()
