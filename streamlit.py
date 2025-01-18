12c96e3f49bbc35b0c28bc7d52b7235ab96ec39c
YounSangMu <37579735+YounSangMu@users.noreply.github.com>
2025-01-18 21:26:58 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -60,7 +60,7 @@ st.markdown('#### *"좋아하는 작가를 만난다는 것"*')
 st.markdown(f"한동안 한 명의 작가에게 빠진다는 건, 잠시 인생을 함께 걸을 동행을 만나는 것 같다는 생각이 들어요.<br>핵심 작품을 엮은 한 권의 책으로, 인생을 섬세하게 느끼고 치열하게 고민했던 작가와 만나는 시간 가지길 바랍니다.", unsafe_allow_html=True)
 
 img = Image.open('image/main_image.png')
-st.image(img, width=300, caption='Image from Unsplash', use_container_width=True)
+st.image(img, width=300, use_container_width=True)
 
 
 #2. 서비스 & 취지 소개 
